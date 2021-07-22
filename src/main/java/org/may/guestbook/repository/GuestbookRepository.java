a492fb47ad5b55160a4c73dac2d9ab0baa280346
jhoon0529 <jhoon0529@icloud.com>
2021-07-22 17:55:05 +0900

    Repository add QuerydslpPredicateExecutor

--- src/main/java/org/may/guestbook/repository/GuestbookRepository.java
+++ src/main/java/org/may/guestbook/repository/GuestbookRepository.java
@@ -2,7 +2,9 @@ package org.may.guestbook.repository;
 
 import org.may.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository 
+		extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 
 }
